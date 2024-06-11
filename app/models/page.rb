522e659d1aee418d26000c3d34432a8b37890491
Patricio <patriciopherrero@gmail.com>
2024-06-11 13:02:36 +0200

    added logic to check if products in stock and some styling for easier ui

---
+++ app/models/page.rb
@@ -0,0 +1,26 @@
+class Page < ApplicationRecord
+  has_many :results, dependent: :destroy
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :check_type, presence: true
+  validates :selector, presence: true
+  validates :match_text, presence: {if: :check_text?}
+
+  def check_text?
+    check_type == "text"
+  end
+
+  def run_check!
+    scraper = Scraper.new(url)
+    result = case check_type
+              when "text"
+                scraper.text(selector: selector).downcase == match_text.downcase
+              when "exists"
+                scraper.present?
+              when "not_exists"
+                !scraper.present?
+              end
+    results.create(success: result)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_10_125857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "pages", force: :cascade do |t|
+    t.string "url"
+    t.string "check_type"
+    t.string "selector"
+    t.string "match_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.bigint "page_id", null: false
+    t.boolean "success"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["page_id"], name: "index_results_on_page_id"
+  end
+
+  add_foreign_key "results", "pages"
 end
